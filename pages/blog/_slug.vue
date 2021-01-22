7e0f2da3cac26c4f866a0a6283eb4de9437cf402
toxicmuffin <toxicmuffin101@hotmail.com>
2021-01-21 20:55:48 -0600

    style: touching up some stuff

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,11 +1,13 @@
 <template>
   <article class="round-pads">
-    <div class="mb-4 mx-auto lg:w-10/12">
-      <div class="inlay round-pads mb-4">
-        <h1 class="mode-text-primary text-3xl font-bold">{{ article.title }}</h1>
-        <h2 class="text-2xl mb-4">{{ article.description }}</h2>
-        <Author :author="author" :date="article.createdAt" :ttr="article.body" />
-        <img class="object-cover mb-4 h-32 md:h-64 w-screen" :src="article.hero_image" :alt="article.alt_tag">
+    <div class="mb-4 mx-auto">
+      <div class="inlay round-pads mode-bg mb-8 mx-auto xl:w-9/12">
+        <div class="round-pads mode-bg-second mb-4">
+          <h1 class="mode-text-primary text-3xl font-bold">{{ article.title }}</h1>
+          <h2 class="text-2xl mb-4">{{ article.description }}</h2>
+        </div>
+        <Author :author="author" :date="article.createdAt" :ttr="article.body" class="mode-bg-second round-pads md:w-1/2"/>
+        <img class="object-cover mb-4 w-screen" :src="article.hero_image" :alt="article.alt_tag">
       </div>
       <nuxt-content :document="article" class="blog-content prose-lg xl:prose-2xl"/>
       <div class="flex flex-row justify-center mb-8">
